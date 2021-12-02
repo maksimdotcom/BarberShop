24b2197beb9c029eb9dbaab3f140a054d20ab89f
maksimdotcom <ya.maksim0511@gmail.com>
2021-12-02 19:16:43 +0300

    init app

---
+++ app.rb
@@ -0,0 +1,63 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+
+get '/about' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+ 
+post '/visit' do 
+	@user_name = params[:user_name]
+	@phone     = params[:phone]
+	@date_time = params[:date_time]
+	@barber    = params[:barber]
+	@color     = params[:color]
+
+	hh = {
+		:user_name => 'Введите имя',
+		:phone     => 'Введите телефон',
+		:date_time => 'Введите дату и время'
+	}
+
+	@error = hh.select {|key,_|params[key] == ""}.values.join(",")
+
+	if @error != ''
+			return erb :visit
+	end
+
+	@title = 'Thank you!'
+	@message = "Уважаемый #{@user_name}, мы ждём вас #{@date_time}"
+
+	f = File.open './public/users.txt', 'a'
+	f.write "User: #{@user_name}, Phone: #{@phone}, Dte and time: #{@date_time}, Baber: #{@barber}, Color: #{@color}.\n"
+	f.close
+
+	erb :welcome
+end
+
+get '/contacts' do 
+	erb :contacts
+end
+
+post '/contacts' do
+	@contact_email = params[:contact_email]
+	@contact_message = params[:contact_message]
+
+	f = File.open './public/contacts.txt', 'a'
+	f.write "Contact: #{@contact_email}, Message: #{@contact_message}.\n"
+	f.close
+
+	@title = 'Спасибо'
+	@message = 'уауа'
+		erb :welcome
+end
