9a0da31b80dc926137e6cd95679c81b434776011
maksimdotcom <ya.maksim0511@gmail.com>
2021-12-04 20:15:09 +0300

    init Barbers table on configure

--- app.rb
+++ app.rb
@@ -4,6 +4,20 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+	db.execute('select * from Barbers where name=?',[name]).length > 0
+end
+
+def seed_db db, barbers 
+
+	barbers.each do |barber|
+		if !is_barber_exists? db, barber
+			db.execute 'insert into Barbers (name) values (?)', [barber]
+		end
+	end
+
+end
+
 def get_db
 	db = SQLite3::Database.new 'barbershop.db'
 	db.results_as_hash = true
@@ -22,7 +36,15 @@ configure do
 			"barber" TEXT,
 			"color" TEXT
 		)'
- end
+
+	db.execute 'CREATE TABLE IF NOT EXISTS
+		"Barbers"
+		(
+			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+			"name" TEXT
+		)'
+	seed_db db, ['Walter White','Jessie Pinkman','Gus Fring', 'Mike Ehrman']
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
